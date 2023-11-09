857da9e966d01eae9550f15a9c19df514d8915aa
devlucascalado <devlucascalado@gmail.com>
2023-11-09 19:16:46 -0300

    exercicio4

---
+++ exercicio4.py
@@ -0,0 +1,11 @@
+#4.Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+nota1 = float(input("Digite a nota 1: "))
+
+nota2 = float(input("Digite a nota 2: "))
+
+nota3 = float(input("Digite a nota 3: "))
+
+nota4 = float(input("Digite a nota 4: "))
+
+print((nota1 + nota2 + nota3 + nota4) / 4)
