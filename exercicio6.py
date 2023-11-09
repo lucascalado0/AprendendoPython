36bd0a4da5f3cfad8f5bf827f8802d270b8c351c
devlucascalado <devlucascalado@gmail.com>
2023-11-09 19:33:28 -0300

    exercicio6

---
+++ exercicio6.py
@@ -0,0 +1,8 @@
+#6.Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+PI = 3.14 #Neste caso, a variável PI tem função de constante
+
+raio = float(input("Informe o raio do círculo: "))
+
+print("A área do circulo é: ", PI * (raio ** 2))
+
