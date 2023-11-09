100134086df56f1acefdbb2a42b129cdcf9b7dae
devlucascalado <devlucascalado@gmail.com>
2023-11-09 19:50:33 -0300

    exercicio9

---
+++ exercicio9.py
@@ -0,0 +1,7 @@
+#9.Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius. C = 5 * ((F-32) / 9)
+
+fahrenheit = float(input("Informe a temperatura em Fahrenheit: "))
+
+C = 5 * ((fahrenheit - 32) / 9)
+
+print("A temperatura correspondente em celcius é: ", C)
