0ef278b30682dcb55927e3d6783f5629197f49d7
devlucascalado <devlucascalado@gmail.com>
2023-11-09 19:10:45 -0300

    exercicio3

---
+++ exercicio3.py
@@ -0,0 +1,7 @@
+#3.Faça um Programa que peça dois números e imprima a soma.
+
+numero1 = int(input("Informe um número: "))
+
+numero2 = int(input("Agora informe outro número: "))
+
+print(numero1 + numero2)
