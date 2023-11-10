e3faf57ae7fce4f51b386093222d4f614908cb17
devlucascalado <devlucascalado@gmail.com>
2023-11-10 09:08:21 -0300

    exercicio10

---
+++ exercicio10.py
@@ -0,0 +1,7 @@
+#10.Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+
+celsius = float(input("Informe a temperatura em graus Celsius: "))
+
+fahrenheit = (celsius * 1.8) + 32
+
+print("O resultado da conversão é: ", fahrenheit)
