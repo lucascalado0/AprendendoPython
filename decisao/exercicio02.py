8f5dceeab08d7cf7582c8589305a38b50e3ce6af
devlucascalado <devlucascalado@gmail.com>
2023-11-14 23:36:06 -0300

    exericio02

---
+++ decisao/exercicio02.py
@@ -0,0 +1,14 @@
+#2.Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+numero = int(input("Informe um valor qualquer: "))
+
+if numero < 0:
+    print("Este número é negativo!")
+
+elif numero > 0:
+    print("Este número é positivo!")
+
+else: 
+    print("Você digitou o número 0!")    
+
+
