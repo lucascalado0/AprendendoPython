93a0038e75324df497592181465f4020bb782426
devlucascalado <devlucascalado@gmail.com>
2023-11-20 13:20:36 -0300

    exercicio14

---
+++ decisao/exercicio14.py
@@ -0,0 +1,32 @@
+"""
+14.Faça um programa que lê as duas notas parciais obtidas por um aluno numa disciplina ao longo de um semestre, e calcule a sua média.
+ A atribuição de conceitos obedece à tabela abaixo: Média de Aproveitamento  Conceito
+  Entre 9.0 e 10.0        A
+  Entre 7.5 e 9.0         B
+  Entre 6.0 e 7.5         C
+  Entre 4.0 e 6.0         D
+  Entre 4.0 e zero        E
+O algoritmo deve mostrar na tela as notas, a média, o conceito correspondente e a mensagem “APROVADO” se o conceito for A, B ou C ou “REPROVADO” 
+se o conceito for D ou E.
+"""
+
+nota1 = float(input("Informe a sua primeira nota: "))
+
+nota2 = float(input("Informe a sua segunda nota: "))
+
+media = (nota1 + nota2) / 2
+
+if 9 <= media <= 10:
+    print("A")
+
+elif 7.5 <= media < 9:
+    print("B")
+
+elif 6 <= media < 7.5:
+    print("C")
+
+elif 4 <= media < 6:
+    print("D")
+
+else:
+    print("E")
